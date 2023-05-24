87d253b036b2997a6f288980d90bce6f7271e011
mromerolabelgrup <mromero@labelgrup.com>
2023-05-24 18:12:10 +0200

    Now, when user navigates, pages alwauys scrolls to top thanks to ScrollToTop.tsx helper.

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import './App.css'
 import { Route, Routes, BrowserRouter, RouteObject } from 'react-router-dom'
 import ROUTES from './infrastructure/constants/routes'
 import Header from './components/Header'
+import ScrollToTop from './infrastructure/helpers/ScrollToTop'
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <div className={'body mb-10 flex flex-col gap-8 px-4'}>
         <Header />
         <main>
