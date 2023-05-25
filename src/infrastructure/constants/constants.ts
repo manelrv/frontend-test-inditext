6508706a3eecdd67d0013c2bf158ce2207877b95
mromerolabelgrup <mromero@labelgrup.com>
2023-05-25 16:34:53 +0200

    now refresh intervals works properly

--- src/infrastructure/constants/constants.ts
+++ src/infrastructure/constants/constants.ts
@@ -1,5 +1,4 @@
 const mode = import.meta.env.VITE_MODE
-console.log({ mode })
 export const MILLISECONDS_IN_HOUR = 3600000
 export const DELAY_IN_HOURS_REFRESH_ALL_PODCASTS = 24
 export const DELAY_IN_HOURS_REFRESH_PODCAST_DETAILS = 24
